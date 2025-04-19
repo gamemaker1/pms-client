e6de60ffbcce13e6b9de6e1f8ea63c4597dfd13c
Vedant K <gamemaker0042@gmail.com>
2025-04-20 01:39:05 +0530

    chore: compile entry esp

--- client.ino
+++ client.ino
@@ -6,8 +6,8 @@
 #include <LiquidCrystal_I2C.h>
 
 // entry, exit, slots
-// #define MODE "entry"
-#define MODE "exit"
+#define MODE "entry"
+// #define MODE "exit"
 
 void setup() {
   Serial.begin(9600);
